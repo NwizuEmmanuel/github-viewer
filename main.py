5f9e2cb91614b945fbace1987898dd0a282fb1fa
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-11-28 17:05:50 +0800

    implement api

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import requests
+import json
 
-url = 'https://api.quotable.io/random'
+def show_activity(data):
+    counter = 1
+    for item in data[:20]:
+        type = item['type']
+        created_at = item['created_at']
+        repo = item['repo']['name']
 
-response = requests.get(url)
+        if "commits" in item['payload']:
+            count_commits = len(item['payload']['commits'])
+        print(f"{counter}. type: {type}, created_at: {created_at}, repo: {repo}, commits: {count_commits or 0}")
+        count_commits = 0
+        counter += 1
 
-if response.status_code == 200:
-    data = response.json()
-    print(f"Quote: {data["quote"]}")
-    print(f"Author: {data["author"]}")
-else:
-    print("Request has failed.")
+def main():
+    username = "NwizuEmmanuel"
+    url = f'https://api.github.com/users/{username}/events'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        show_activity(data=data)
+    else:
+        print("Request has failed.")
+
+if __name__ == "__main__":
+    main()
